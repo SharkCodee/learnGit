f4b59b5b6952e3827f5aebe805b4a543bf3b11a2
xp <xp>
2017-09-19 10:28:41 +0800

    first to git

---
+++ index.js
@@ -0,0 +1,32 @@
+document.getElementById('xp').addEventListener('keyup', function(e) {
+    var targe = e.target
+    console.log(e);
+    targe.value = (targe.value.match(/\d{0,6}(\.\d{0,2})?/g))[0];
+})
+
+var objec = {
+    "name": "ricky",
+    "age": 18,
+    toJSON: function(){
+        return this.name
+    }
+}
+var str = "ricky";
+
+let xp = document.getElementById('xpp');
+console.log(xp);
+xp.innerHTML = "888";
+
+try {
+var base = Base64;
+
+} catch (error) {
+    console.log(error);
+}
+document.cookie = "123";
+alert(document.cookie);
+// alert(xp.getAttribute("value"));
+
+// alert(JSON.stringify(objec, null, 10));
+// alert(JSON.stringify(objec));
+
